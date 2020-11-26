46304e81f6a82d3dfbe9b0d694bc0773fbdc185b
Nikolay Elmanov <nikelm@yandex.ru>
2020-11-26 16:43:21 +0300

    feat: create GET requests

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const fs = require('fs');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
+app.use(express.static(`${__dirname}/public`));
 
-    console.log(`App listening on port ${PORT}`);
-})
+app.get('/users', (req, res) => {
+  fs.readFile('user.json', { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.type('json').send(data);
+  });
+});
+
+app.get('/cards', (req, res) => {
+  fs.readFile('cards.json', { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.type('json').send(data);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  fs.readFile('user.json', { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+    res.type('json').send(req.params.id);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
